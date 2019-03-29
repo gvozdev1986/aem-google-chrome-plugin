7659fd4c34b3aabe4520bf70a5a79cb4f4e0016b
Aliaksandr <aliaksandr.hvozdzeu@gmail.com>
2019-03-29 22:23:11 +0300

    Update page settings

--- aemer/js/popup.js
+++ aemer/js/popup.js
@@ -28,15 +28,22 @@ document.getElementById("settingsBtn").addEventListener("click", function () {
 setInterval(function () {
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', host + ":" + port + "/", false);
+    xhr.open('GET', host + ":" + port + "/");
+
+    xhr.onprogress = function () {
+
+        if (xhr.status === 200) {
+            document.getElementById('imageStatus').src = 'image/online.png';
+            chrome.browserAction.setBadgeText({text:"ON"});
+            chrome.browserAction.setBadgeBackgroundColor({color: "green"});
+        } else {
+            document.getElementById('imageStatus').src = 'image/offline.png';
+            chrome.browserAction.setBadgeText({text:"OFF"});
+            chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+        }
+
+    };
+
     xhr.send();
 
-    if (xhr.status === 200) {
-        document.getElementById('imageStatus').src = 'image/online.png';
-        chrome.browserAction.setBadgeText({text:"ON"});
-    } else {
-        document.getElementById('imageStatus').src = 'image/offline.png';
-        chrome.browserAction.setBadgeText({text:"OFF"});
-    }
-
-});
+}, 100);
